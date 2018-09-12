6958d5c464cc7d98b365f61d03e0a42fd84c7624
Md Omer Arafat <arafatomer66@gmail.com>
2018-09-12 16:22:38 +0600

    Final project skeleton

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,14 +1,11 @@
 export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
 
-    public name : string ;
-    public description : string ;
-    public imagePath : string ;
-
-    constructor(name : string , des :string ,imagePath : string ){
-        this.name = name;
-        this.description = des ;
-        this.imagePath = imagePath ;
-
-
-    }
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
 }
