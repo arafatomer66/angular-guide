459380d11a8f4362d839da8f3bd2bde1ed3469d4
Md Omer Arafat <arafatomer66@gmail.com>
2018-09-22 13:17:43 +0600

    Removing the bindings between recipes 3lvl binding with injecting recipe service

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -10,9 +10,31 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe :Recipe ;
-  constructor() { }
+  constructor(private recipeService : RecipeService) { 
+
+   }
 
   ngOnInit() {
+    this.recipeService.recipeSelected
+    .subscribe(
+       ( recipe : Recipe ) => {
+         this.selectedRecipe = recipe
+       }
+    );
+
   }
 
 }
+
+// ES6 syntax : 
+
+// ( recipe : Recipe ) => {
+//   this.selectedRecipe = recipe
+// }
+
+// ( recipe : Recipe ) is argument and 
+
+// {
+//   this.selectedRecipe = recipe
+// }      is the function
+
