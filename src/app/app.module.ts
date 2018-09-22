2821b5105e5345c6eb8c9bb281dc1eaa7d8faa12
arafat <arafatomer66@gmail.com>
2018-09-22 22:20:53 +0600

    Initial routing project setup (side project : routing component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -29,13 +28,19 @@ import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RoutingComponent } from './routing/routing.component';
 import { HomeComponent } from './routing/home/home.component';
-
 import { ServersComponent } from './routing/servers/servers.component';
 import { EditServerComponent } from './routing/servers/edit-server/edit-server.component';
 import { UsersComponent } from './routing/users/users.component';
 import { UserComponent } from './routing/users/user/user.component';
 import { ServersService } from './routing/servers/servers.service';
 import { ServerComponent } from './routing/servers/server/server.component';
+import { Routes ,RouterModule  } from '@angular/router';
+
+const appRoutes : Routes = [
+  { path : '' , component : HomeComponent },
+  { path : 'users' , component : UserComponent },
+  { path : 'servers' , component : ServersComponent }
+]; 
 
 @NgModule({
   declarations: [
@@ -70,7 +75,8 @@ import { ServerComponent } from './routing/servers/server/server.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AccountsService , LoggingService ,RecipeService , ShoppingListService ,ServersService ],
   bootstrap: [AppComponent]
