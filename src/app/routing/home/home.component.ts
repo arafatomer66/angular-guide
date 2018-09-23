33ae11c0d15dc00657a27fc1a656708ff9695fd6
Md Omer Arafat <arafatomer66@gmail.com>
2018-09-23 10:40:29 +0600

    Navigating another page with router injection and navigate()

--- src/app/routing/home/home.component.ts
+++ src/app/routing/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router :Router ) { }
 
   ngOnInit() {
   }
 
+  onLoadServer(){
+    //complex calculation
+    this.router.navigate(['/servers']);
+  }
+
 }
+
+
+// navigating to another page after doing complex calcultion (just like header in php)
