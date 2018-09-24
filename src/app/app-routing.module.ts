6c0d091f22c83eb5897d8dc8c8a981c68c2eb29d
arafat <arafatomer66@gmail.com>
2018-09-24 22:47:56 +0600

    Routing for nav-bar !

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import {   NgModule   } from '@angular/core' ;
 import { RouterModule , Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ServicesComponent } from './services/services.component';
+import { DirectiveComponent } from './directive/directive.component';
  
 
 const appRoutes : Routes = [
     { path : '' , redirectTo : '/recipes' , pathMatch : 'full' } ,
     { path : 'recipes' , component : RecipesComponent} ,
-    { path : 'shopping-list' , component : ShoppingListComponent} 
+    { path : 'shopping-list' , component : ShoppingListComponent},
+    { path : 'services' , component : ServicesComponent} ,
+    { path : 'directive' , component : DirectiveComponent} 
 ]
 @NgModule({
     imports : [RouterModule.forRoot(appRoutes) ],
