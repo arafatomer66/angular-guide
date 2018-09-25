12295f2488aea74e03d69b9e1aee1609033e23fb
Md Omer Arafat <arafatomer66@gmail.com>
2018-09-25 14:43:09 +0600

    Custom observable : using observer (next , error , complete)

--- src/app/observable/home/home.component.ts
+++ src/app/observable/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { interval } from "rxjs";
+import { interval, Observer, Observable } from "rxjs";
+import { create } from 'domain';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,13 +12,33 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-     const myNumbers = interval(1000);
-     myNumbers.subscribe(
-       (number : number) => 
-       {
-         console.log(number);
-       }
-     );
+    //  const myNumbers = interval(1000);
+    //  myNumbers.subscribe(
+    //    (number : number) => 
+    //    {
+
+    
+    //      console.log(number);
+    //    }
+    //  );
+
+     const myObservables = Observable.create((observer : Observer<string>) => {
+        setTimeout(()=> observer.next('First package'),2000);
+        setTimeout(()=> observer.next('second package'),4000);
+        setTimeout(()=> observer.error('error package'),6000);
+        setTimeout(()=> observer.complete(),6000);
+    });
+    myObservables.subscribe(
+      (data :string) => {
+        console.log(data);
+      },
+      (error :string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('completed');
+      },
+    );
   }
 
 }
