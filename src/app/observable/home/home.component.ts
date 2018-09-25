916022ef9a57e3f929a8ac9d5269bf42bd6617ba
Md Omer Arafat <arafatomer66@gmail.com>
2018-09-25 15:58:01 +0600

    Observable with map operator

--- src/app/observable/home/home.component.ts
+++ src/app/observable/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit , OnDestroy  } from '@angular/core';
-
 import { interval, Observer, Observable, Subscription } from "rxjs";
-import { create } from 'domain';
+import 'rxjs/Rx' ;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,6 +13,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
        const myNumbers = interval(1000);
+      //  .map(()=>
+      //  {
+
+      //  });
       this.numberSub = myNumbers.subscribe(
        (number : number) => 
        {
