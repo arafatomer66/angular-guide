64c2004e2b9b220336ad49f5964f43acc889317e
Md Omer Arafat <arafatomer66@gmail.com>
2018-09-25 13:34:28 +0600

    Custom observable : with interval

--- src/app/observable/home/home.component.ts
+++ src/app/observable/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { interval } from "rxjs";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +11,13 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+     const myNumbers = interval(1000);
+     myNumbers.subscribe(
+       (number : number) => 
+       {
+         console.log(number);
+       }
+     );
   }
 
 }
