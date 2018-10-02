2c6eb010bd223e72d76133c4a1bfba885947279c
Md Omer Arafat <arafatomer66@gmail.com>
2018-10-02 12:29:26 +0600

    Reactive form : recipe edit with inline items

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -69,6 +69,16 @@ export class RecipeEditComponent implements OnInit {
   }
 
 
+  onAddIngredient(){
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name' : new FormControl(),
+        'amount' : new FormControl()
+      })
+    );
+  }
+
+
   onSubmit(){
     console.log(this.recipeForm);
   }
