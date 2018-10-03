db6753e2b7508a131755261d3b861d8618e7e17a
Md Omer Arafat <arafatomer66@gmail.com>
2018-10-03 11:54:07 +0600

    Http side project ends

--- src/app/http/server.service.ts
+++ src/app/http/server.service.ts
@@ -24,10 +24,13 @@ export class ServerService {
     return this.http.get('https://ng-recipe-book-866d5.firebaseio.com/data.json').pipe(map(
        (response : Response)=> {
          const data = response.json();
+         for(const server of data){
+            server.name = 'FETCHED_' + server.name ;
+         }
          return data ;
        }
      ) 
-    )
+    );
   }
 
 }
